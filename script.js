9fffb6fbccb8606c4085bf6ac5e74dccb75a22cd
Alina Turdakunova <141915076+Alinatd@users.noreply.github.com>
2023-09-06 21:02:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const date = document.getElementById('date');
+const day= document.getElementById('day');
+const month= document.getElementById('month');
+const year= document.getElementById('year');
+
+
+ 
+const today = new Date()
+const weekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+const allMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"]
